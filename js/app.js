515d54383b80c957cd3d59dd4611bc69bd0f7c89
MuhammedAlaa00 <36922989+mohamedalaa00@users.noreply.github.com>
2022-07-13 18:42:42 +0200

    update navbar style, added active class option

--- js/app.js
+++ js/app.js
@@ -4,38 +4,36 @@
 /**
  * Start of Global Variables
  */
-let navItemsArr = ["section 1", "section 2", "section 3", "section 4"];
+const navItemsArr = ["section 1", "section 2", "section 3", "section 4"];
 /**
  * End of Global Variables
  */
 /**
- * Create Navigation Element function
+ * Start of Create Navigation Element function
  */
 // edit the css style in the css file.
 window.addEventListener("load", () => {
-  document.body.style.margin = "0";
-  document.body.style.backgroundColor = "#000034";
   const Navbar = document.createElement("nav");
-  Navbar.style.display = "flex";
-  Navbar.style.justifyContent = "end";
-  Navbar.style.width = "100%";
-  Navbar.style.backgroundColor = "#fff";
   document.body.append(Navbar);
-  const ulUnorderlist = document.createElement("ul");
-  ulUnorderlist.style.display = "flex";
-  ulUnorderlist.style.padding = "0 1rem"
-  Navbar.appendChild(ulUnorderlist);
+  const ulUnorderList = document.createElement("ul");
+  Navbar.appendChild(ulUnorderList);
   for (let index = 0; index < navItemsArr.length; index++) {
     const el = document.createElement("li");
     el.innerHTML = navItemsArr[index];
     el.setAttribute("id", navItemsArr[index]);
     el.setAttribute("class", "navItem");
-    const firstListItemElement = document.getElementsByClassName("navItem");
-    firstListItemElement[0]?.classList.add("active");
-    el.style.listStyle = "none";
-    el.style.color = "#000";
-    el.style.padding = "1rem";
-    ulUnorderlist.appendChild(el);
+    const firstListItemElement = document.getElementsByClassName("navItem")[0];
+    firstListItemElement?.classList.add("active");
+    ulUnorderList.appendChild(el);
   }
-  console.log(Navbar);
+  let navItems = document.querySelectorAll(".navItem");
+  navItems.forEach((el) => {
+    el.addEventListener("click", () => {
+      document.querySelector(".active").classList.remove("active");
+      el.classList.add("active");
+    });
+  });
 });
+/**
+ * End of Create Navigation Element function
+ */
